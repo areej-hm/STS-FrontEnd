a2d5274fdc097401872e6fa8cdc0df714c3c5df2
areejmohammad <areejhmohammad@outlook.com>
2021-03-08 13:30:00 +0200

    4commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Employee } from './Models/Employee';
-import { EmployeeServiceService } from './services/employee-service.service';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +6,6 @@ import { EmployeeServiceService } from './services/employee-service.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'taskForSteadyPoint';
   constructor(){
 
   }
